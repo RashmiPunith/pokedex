27f5529bf16fbde9779fc602702c152a79f3939c
rashmipunith <rhegde567@gmail.com>
2022-01-22 16:08:45 -0500

    Saying Hello world
    edit some paragraph

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Rashmi
         </p>
         <a
           className="App-link"
